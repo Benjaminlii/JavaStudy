9cba26877b3db6cbf6314788ded9d0c542a822b8
Benjamin <305193188@qq.com>
2018-12-12 13:53:09 +0800

    多线程：Thread子类创建自定义线程

---
+++ src/textMultithread/textThread.java
@@ -0,0 +1,43 @@
+package textMultithread;
+
+/**
+ * 测试使用继承Thread类进行线程的创建
+ *
+ * 通过创建一个Thread类的子类来创建一个线程
+ * 线程的任务放在该子类的run方法中，相当于主线程的main方法
+ * 线程创建之后并不会运行，调用线程的run方法也不会启动线程
+ * 需要调用子类的start方法启动线程
+ * start方法会通知JVM（虚拟机）开启线程并在自定义的线程中执行run方法
+ *
+ * 看样子我电脑CPU跑的还挺快
+ *
+ * author:Benjamin
+ * date:2018.12.12
+ */
+
+class Thread1 extends Thread{
+    private String name;
+
+    Thread1(String name) {
+        this.name = name;
+    }
+
+    // run方法承载了自定义线程中要运行的代码
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++){
+            System.out.println(this.name + "---" + i);
+        }
+    }
+}
+
+public class textThread {
+    public static void main(String[] args) {
+        // 创建线程，这时内存中并没有自定义的线程，只是一定定义好了
+        // 在调用start方法之后，才会有三个线程（主线程、one、two）
+        Thread1 one = new Thread1("one");
+        Thread1 two = new Thread1("two");
+        one.start();
+        two.start();
+    }
+}
