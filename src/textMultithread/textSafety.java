be7542e7310e9245886a9927dc5de82a29b23ca9
Benjamin <305193188@qq.com>
2018-12-13 14:33:53 +0800

    通过使用线程锁synchronized解决线程安全问题

---
+++ src/textMultithread/textSafety.java
@@ -0,0 +1,63 @@
+package textMultithread;
+
+/**
+ * 用于测试怎样解决线程安全问题
+ *
+ * 使用线程锁将一段操作共享数据的代码锁起来，使线程运行期间其他线程不能被运行
+ * 原理为，一个线程进入synchronized后的代码块时，obj对象会被标记
+ * 当CPU切换到其他线程时，线程看到被标记的obj，就会停止执行，随后被CPU切换
+ * 原线程在数次被CPU切换到已经进入synchronized语句的代码块中并且运行完之后
+ * 在退出synchronized代码块时，会将obj的标记还原，使得其他线程有机会进入synchronized语句
+ *
+ * author:Benjamin
+ * date:2018.12.13
+ */
+
+public class textSafety {
+    public static void main(String[] args) {
+        Runnable r1 = new Ticket3();
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r1);
+        Thread t3 = new Thread(r1);
+        Thread t4 = new Thread(r1);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
+
+// 这里的类与之前的类相同，只添加了线程安全的处理
+class Ticket3 implements Runnable {
+    // 票不能重复卖，所以四个Ticket对象共用一个num，及静态变量
+    private int num = 100;
+    private final Object obj = new Object();
+
+    private void sale() throws InterruptedException {
+        // 在这个例子中，不能将run方法中的所有代码锁起来，所以更改了循环的结构
+        while (true) {
+            //
+            synchronized (obj) {
+                // 保证票为正数
+                if (this.num > 0) {
+                    // 延时
+                    Thread.sleep(10);
+                    System.out.println(Thread.currentThread().getName() + "...." + num--);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            sale();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
