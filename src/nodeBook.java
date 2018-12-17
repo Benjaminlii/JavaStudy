9644cc5761598cf3dbc09250be8bcdda20062e0c
Benjamin <305193188@qq.com>
2018-12-17 23:40:49 +0800

    Java版的GUI计算器

--- src/nodeBook.java
+++ src/nodeBook.java
@@ -6,5 +6,16 @@
 public class nodeBook {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String s;
+        s = "ak*js/dhfkjasdhf";
+        String[] a = s.split("^");
+        for(String b : a){
+            System.out.println(b);
+        }
+
+        double d = 12.3%1.2;
+        System.out.println(d);
+
     }
 }
