00d46fd80bbbf9039c247c76cacc609f8aa99b5d
Benjamin <305193188@qq.com>
2018-12-19 23:29:57 +0800

    PTA

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,82 @@
-import java.math.BigInteger;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 /**
- * 草稿纸
- * <p>
+ * 与PTA上的运算结果不一致，小数点后15位的地方小了一点
+ * 算法肯定没问题，不然不可能差这么少
  * author:Benjamin
  */
 
-public class Main {
-    public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
+class Point {
+    private double x;
+    private double y;
+    private boolean flag = false;
 
-        LinkedList<Long> one = new LinkedList<>();
-        long num = (long)Math.sqrt(Long.MAX_VALUE)+1;
-        BigInteger big = new BigInteger(String.valueOf(num));
-        BigInteger numberOfOne = new BigInteger("1");
-        for(int i = 0; i < 10; i++){
-            System.out.println(big.pow(2));
-            big = big.add(numberOfOne);
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Point(line l1, line l2) {
+        if(l1.getK() == l2.getK()){
+            this.flag = false;
+        }else {
+            this.flag = true;
+            this.x = (l2.getB() - l1.getB()) / (l1.getK() - l2.getK());
+            this.y = this.x * l1.getK() + l1.getB();
         }
+    }
 
-//        in.close();
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    void printPoint() {
+        if(this.flag){
+            System.out.println("The intersecting point is at (" + this.x + ", " + this.y + ")");
+        }else{
+            System.out.println("The two lines are parallel");
+        }
+    }
+}
+
+class line {
+    private double k;
+    private double b;
+
+    line(Point p1, Point p2) {
+        this.k = (p1.getY() - p2.getY()) / (p1.getX() - p2.getX());
+        this.b = p1.getY() - this.k * p1.getX();
+    }
+
+    double getK() {
+        return k;
+    }
+
+    double getB() {
+        return b;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Point p1 = new Point(in.nextDouble(), in.nextDouble());
+        Point p2 = new Point(in.nextDouble(), in.nextDouble());
+        Point p3 = new Point(in.nextDouble(), in.nextDouble());
+        Point p4 = new Point(in.nextDouble(), in.nextDouble());
+
+        line l1 = new line(p1, p2);
+        line l2 = new line(p3, p4);
+
+        Point interPoint = new Point(l1, l2);
+        interPoint.printPoint();
+
+        in.close();
     }
 }
