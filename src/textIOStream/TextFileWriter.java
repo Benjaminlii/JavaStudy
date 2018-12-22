aac1b550bca123b3cc111380c29bfd59af216630
Benjamin <305193188@qq.com>
2018-12-22 20:47:21 +0800

    续写和异常处理

--- src/textIOStream/TextFileWriter.java
+++ src/textIOStream/TextFileWriter.java
@@ -17,6 +17,7 @@ import java.util.Scanner;
  * 先从字符流开始学习
  * FileWriter类（继承关系：Object->Writer->OutputStreamWriter->FileWriter）
  * 构造器： FileWriter(String FileName);    创建名为FileName的文件写入流，有则覆盖。
+ *          FileWriter(String FileName, boolean append);    append表示是否为续写
  * 方法     FileWriter.flush();             刷新缓冲区域，将缓冲区域中的字符写入文件中。
  *          FileWriter.close();             关闭流，在关闭之前会进行刷新操作。
  *
@@ -32,6 +33,10 @@ class TxtWriter{
         this.fw = new FileWriter(fileName);
     }
 
+    TxtWriter(String fileName, boolean isAppend) throws IOException {
+        this.fw = new FileWriter(fileName, isAppend);
+    }
+
     void write(String data) throws IOException {
         this.fw.write(data);
         this.fw.flush();
@@ -42,7 +47,11 @@ public class TextFileWriter {
 
     public static void main(String[] args) {
         try(Scanner in = new Scanner(System.in)){
-            TxtWriter one = new TxtWriter("src\\textIOStream\\one.txt");
+            // JDK1.7之后try的新特性，可以在try语句中添加局部变量，退出try之后会自动close，需要实现Closeable接口
+            // 在JDK1.7之前，IO流部分的异常处理非常麻烦
+            // 要在try外部创建变量，try内部初始化
+            // 而且对象的close要放在finally中，并且在close之前判断是否为空指针（创建时报异常导致初始化未成功）
+            TxtWriter one = new TxtWriter("src\\textIOStream\\one.txt", true);
             one.write(in.nextLine());
         } catch (IOException e) {
             e.printStackTrace();
