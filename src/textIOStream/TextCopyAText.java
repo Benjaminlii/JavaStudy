4e7be61d715f33db3cf72f8651cab97bc55f8084
Benjamin <305193188@qq.com>
2018-12-24 22:12:34 +0800

    一次性读取多个字符，并根据读取到的个数进行存储

--- src/textIOStream/TextCopyAText.java
+++ src/textIOStream/TextCopyAText.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 /**
  * 复制一个txt文件
+ * 可以一个一个字符读取，并进行存储，也可以一次性读取并且存储多个字符
  * author:Benjamin
  * date:2018.12.24
  */
@@ -15,9 +16,10 @@ public class TextCopyAText {
         try (FileReader read = new FileReader("src\\textIOStream\\one.txt");
              FileWriter write = new FileWriter("src\\textIOStream\\copy.txt")
         ) {
-            int data;
-            while((data = read.read())!=-1){
-                write.write(data);
+            int len;
+            char[] buf = new char[1024];
+            while ((len = read.read(buf)) != -1) {
+                write.write(buf, 0, len);
             }
             write.flush();
         } catch (IOException e) {
