74aa9db19f381bda9180fbdcd03d8491e61cc1ab
Benjamin <305193188@qq.com>
2018-12-24 14:50:12 +0800

    期末练习题4-3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
  * author:Benjamin
+ * date:2018.12.24
  */
 public class Main {
 
+    private static void exchange(char[] array, int one, int two){
+        char tmp = array[one];
+        array[one] = array[two];
+        array[two] = tmp;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BigInteger one = new BigInteger(in.next());
-        BigInteger two = new BigInteger(in.next());
-        System.out.println(one.divide(two));
+        char[]one = in.next().toCharArray();
         in.close();
+
+        for(int i = 0; i < one.length; i++){
+            int flag = i;
+            for(int j = i+1; j<one.length;j++){
+                if((int) one[flag] > (int) one[j]){
+                    flag = j;
+                }
+            }
+            exchange(one,i,flag);
+        }
+
+        System.out.println(one);
     }
 }
