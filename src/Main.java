dad5a9c01e3b000b674ff75003bcd9eba0d315fa
Benjamin <305193188@qq.com>
2018-12-24 21:53:59 +0800

    未填充字符数组的默认值

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-import java.util.Scanner;
 
 /**
  * author:Benjamin
@@ -8,22 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        BigInteger num = new BigInteger(Long.MAX_VALUE + "");
-        BigInteger one = new BigInteger("1");
-        num = num.add(one);// 题目中是大于Long.MAX_VALUE的数
-        BigInteger n = new BigInteger(in.next());
-        int count = 0;
-
-        while (count < 3) {
-            if (num.mod(n).toString().equals("0")) {
-                System.out.println(num);
-                count++;
-            }
-            num = num.add(one);
-        }
-
-        in.close();
+        char[] c = new char[100];
+        System.out.println(c[50] == '\u0000');
     }
 }
