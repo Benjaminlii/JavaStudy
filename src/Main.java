968563203a2ee996ac46b3549cee7a7bc2585128
Benjamin <305193188@qq.com>
2018-12-24 14:01:20 +0800

    期末练习题4-2

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,16 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
  * author:Benjamin
  */
 public class Main {
-    public static Object max(Comparable[] a) {
-        int flag = 0;
-        for(int i = 0;i < a.length; i++){
-            if(a[flag].compareTo(a[i]) < 0){
-                flag = i;
-            }
-        }
-        return a[flag];
-    }
 
     public static void main(String[] args) {
-        String[] sArray = new String[5];
-        Integer[] intArray = new Integer[5];
-        Scanner input = new Scanner(System.in);
-        for(int i=0;i<sArray.length;i++)  {
-            sArray[i] = input.next();
-        }
-
-        for(int i=0;i<intArray.length;i++)  {
-            intArray[i] = new Integer(input.nextInt());
-        }
-
-        System.out.println("Max string is " +(String)max(sArray));
-        System.out.println("Max integer is " + (Integer)max(intArray));
+        Scanner in = new Scanner(System.in);
+        BigInteger one = new BigInteger(in.next());
+        BigInteger two = new BigInteger(in.next());
+        System.out.println(one.divide(two));
+        in.close();
     }
 }
