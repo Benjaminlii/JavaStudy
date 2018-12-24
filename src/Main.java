78b7f6540c37929ecd112ab4e02476066b6272a4
Benjamin <305193188@qq.com>
2018-12-24 16:04:58 +0800

    期末练习题4-11

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.math.BigInteger;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 /**
@@ -10,17 +11,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        BigInteger m = new BigInteger(in.next());
-        BigInteger one = new BigInteger("1");
-        int n = in.nextInt();
-
-        while(n>0){
-            if(m.isProbablePrime(1)){
-                n--;
-                System.out.println(m);
-            }
-            m = m.add(one);
-        }
+        GregorianCalendar oneTime = new GregorianCalendar();
+        oneTime.setTimeInMillis(in.nextLong());
+        System.out.println(oneTime.get(Calendar.YEAR) + "-" +
+                oneTime.get(Calendar.MONTH) + "-" +
+                oneTime.get(Calendar.DAY_OF_MONTH));
 
         in.close();
     }
