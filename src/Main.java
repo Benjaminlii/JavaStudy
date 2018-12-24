28d5459dbbab8d27bb4faff7152d77575a7dc182
Benjamin <305193188@qq.com>
2018-12-24 15:49:41 +0800

    期末练习题4-8

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -6,30 +5,37 @@ import java.util.Scanner;
  * date:2018.12.24
  */
 public class Main {
+    private static boolean isAInt(char[] one) {
+        for (char i : one) {
+            if (!((int) i >= 48 && (int) i <= 57)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isInt(String[] one) {
+        for (String i : one) {
+            if (!isAInt(i.toCharArray())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        HashMap<String, Integer> list = new HashMap<>();
-
-        String name;
-        Integer number;
-
+        String[] one = new String[2];
         while (true) {
-            name = in.next();
-            if (name.equals("noname")) {
+            one[0] = in.next();
+            one[1] = in.next();
+            if (!isInt(one)) {
+                System.out.println("Incorrect input and re-enter two integers:");
+            } else {
+                System.out.println("Sum is " + (Integer.valueOf(one[0]) + Integer.valueOf(one[1])));
                 break;
             }
-            number = in.nextInt();
-            list.put(name, number);
-        }
-
-        name = in.next();
-        number = list.get(name);
-        if (number == null) {
-            System.out.println("Not found.");
-        }else{
-            System.out.printf("%.2f", number*0.21);
         }
 
         in.close();
