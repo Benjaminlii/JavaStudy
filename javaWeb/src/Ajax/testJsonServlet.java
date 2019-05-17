c53cedc26a6542ca999d983526ba8f61ee9cd2f8
Benjamin <305193188@qq.com>
2019-05-17 15:48:53 +0800

    jQuery Ajax

--- javaWeb/src/Ajax/testJsonServlet.java
+++ javaWeb/src/Ajax/testJsonServlet.java
@@ -1,5 +1,8 @@
 package Ajax;
 
+import net.sf.json.JSONObject;
+import studentDemo.entity.Student;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,18 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/testAjaxServlet")
-public class testAjaxServlet extends HttpServlet {
+@WebServlet("/testJsonServlet")
+public class testJsonServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
-        String mobile = request.getParameter("mobile");
-        PrintWriter pw = response.getWriter();
-        if("15991075603".equals(mobile)){
-            pw.print("true");
-        }else{
-            pw.print("false");
-        }
-        pw.close();
+        JSONObject json = new JSONObject();
+        json.put("stu1", new Student("Benjamin", 19, "xa"));
+        json.put("stu2", new Student("lt", 18, "bj"));
+        json.put("stu3", new Student("llf", 20, "kel"));
+
+        PrintWriter out = response.getWriter();
+        System.out.println(json);
+        out.print(json);
+        out.close();
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
